53285caeec094db845c41e132efc673777f22ed1
sihyeong671 <bshlab671@naver.com>
2023-03-10 11:15:59 +0900

    Readme수정 및 step10 완료

--- 박시형/step/step9.py
+++ 박시형/step/step9.py
@@ -1,24 +1,11 @@
 import numpy as np
 
 
-# class Variable:
-#   def __init__(self, data) -> None:
-#     self.data = data
-#     self.grad = None
-#     self.creator = None
-  
-#   def set_creator(self, func):
-#     self.creator = func
-  
-#   def backward(self):
-#     f = self.creator
-#     if f is not None:
-#       x = f.input
-#       x.grad = f.backward(self.grad)
-#       x.backward()
-      
 class Variable:
   def __init__(self, data) -> None:
+    if data is not None:
+      if not isinstance(data, np.ndarray):
+        raise TypeError(f'{type(data)}는 지원하지 않는 타입입니다.')
     self.data = data
     self.grad = None
     self.creator = None
@@ -43,11 +30,16 @@ class Function:
   def __call__(self, input: Variable):
     x = input.data
     y = self.forward(x)
-    output = Variable(y)
+    output = Variable(self._as_array(y))
     output.set_creator(self)
     self.input = input
     self.output = output
     return output
+
+  def _as_array(x):
+    if np.isscalar(x):
+      return np.array(x)
+    return x
   
 class Square(Function):
   def forward(self, x):
@@ -67,29 +59,19 @@ class Exp(Function):
     gx = np.exp(x) * gy
     return gx
   
-  
-if __name__ == "__main__":
-  A = Square()
-  B = Exp()
-  C = Square()
-  
-  x = Variable(np.array(0.5))
-  
-  a = A(x)
-  b = B(a)
-  y = C(b)
+def square(x):
+  return Square()(x)
 
-  assert y.creator == C
-  assert y.creator.input == b
-  assert y.creator.input.creator == B
-  assert y.creator.input.creator.input == a
-  assert y.creator.input.creator.input.creator == A
-  assert y.creator.input.creator.input.creator.input == x
-  
+def exp(x):
+  return Exp()(x)
+
+if __name__ == "__main__":
+  x = Variable(np.array(0.5))
+  # a = square(x)
+  # b = exp(x)
+  # y = square(x)
+  y = square(exp(square(x)))
+
   y.grad = np.array(1.0)
   y.backward()
   print(x.grad)
-  
-  
-  
-  
--- 박시형/step/utils.py
+++ 박시형/step/utils.py
@@ -1,24 +1,11 @@
 import numpy as np
 
 
-# class Variable:
-#   def __init__(self, data) -> None:
-#     self.data = data
-#     self.grad = None
-#     self.creator = None
-  
-#   def set_creator(self, func):
-#     self.creator = func
-  
-#   def backward(self):
-#     f = self.creator
-#     if f is not None:
-#       x = f.input
-#       x.grad = f.backward(self.grad)
-#       x.backward()
-      
 class Variable:
   def __init__(self, data) -> None:
+    if data is not None:
+      if not isinstance(data, np.ndarray):
+        raise TypeError(f'{type(data)}는 지원하지 않는 타입입니다.')
     self.data = data
     self.grad = None
     self.creator = None
@@ -39,15 +26,29 @@ class Variable:
       if x.creator is not None:
         funcs.append(x.creator)
     
+    
 class Function:
   def __call__(self, input: Variable):
     x = input.data
     y = self.forward(x)
-    output = Variable(y)
+    output = Variable(self._as_array(y))
     output.set_creator(self)
     self.input = input
     self.output = output
     return output
+  
+  def forward(self, x):
+    raise NotImplementedError
+
+  def backward(self, gy):
+    raise NotImplementedError
+  
+  @staticmethod
+  def _as_array(x):
+    if np.isscalar(x):
+      return np.array(x)
+    return x
+  
   
 class Square(Function):
   def forward(self, x):
@@ -68,28 +69,17 @@ class Exp(Function):
     return gx
   
   
-if __name__ == "__main__":
-  A = Square()
-  B = Exp()
-  C = Square()
-  
-  x = Variable(np.array(0.5))
-  
-  a = A(x)
-  b = B(a)
-  y = C(b)
+def square(x):
+  return Square()(x)
 
-  assert y.creator == C
-  assert y.creator.input == b
-  assert y.creator.input.creator == B
-  assert y.creator.input.creator.input == a
-  assert y.creator.input.creator.input.creator == A
-  assert y.creator.input.creator.input.creator.input == x
-  
-  y.grad = np.array(1.0)
-  y.backward()
-  print(x.grad)
-  
-  
-  
-  
+
+def exp(x):
+  return Exp()(x)
+
+
+def numerical_diff(f, x: Variable, eps=1e-4):
+  x0 = Variable(x.data - eps)
+  x1 = Variable(x.data + eps)
+  y0 = f(x0)
+  y1 = f(x1)
+  return (y1.data - y0.data) / (2 * eps)
